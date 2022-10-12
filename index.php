9317c43761e930786e7bb3efa63b818c5a704c10
Achoycito <56978465+Achoycito@users.noreply.github.com>
2022-10-12 14:18:52 -0600

    Modificaciones al login, nuevo logo, nuevo banner en login, tematica mercado madero

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
     <!-- custom Css-->
     <link href="<?= BASE_PATH ?>public/css/custom.min.css" rel="stylesheet" type="text/css" />
 
-
 </head>
 
 <body>
@@ -50,11 +49,11 @@
                     <div class="col-lg-12">
                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                             <div>
-                                <a href="index.html" class="d-inline-block auth-logo">
-                                    <img src="<?= BASE_PATH ?>public/images/logo-light.png" alt="" height="20">
+                                <a href="" class="d-inline-block auth-logo">
+                                    <img src="<?= BASE_PATH ?>public/images/logo.png" alt="Logo" height="75">
                                 </a>
                             </div>
-                            <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+                            <p class="mt-3 fs-20 fw-medium">Mercado Francisco I. Madero</p>
                         </div>
                     </div>
                 </div>
@@ -66,48 +65,39 @@
 
                             <div class="card-body p-4">
                                 <div class="text-center mt-2">
-                                    <h5 class="text-primary">Welcome Back !</h5>
-                                    <p class="text-muted">Sign in to continue to Velzon.</p>
+                                    <h5 class="text-primary">¡Bienvenido de vuelta!</h5>
+                                    <p class="text-muted">Inicie sesión para entrar al mercado.</p>
                                 </div>
                                 <div class="p-2 mt-4">
-                                    <form action="https://themesbrand.com/velzon/html/material/index.html">
+                                    <form action="">
 
                                         <div class="mb-3">
-                                            <label for="username" class="form-label">Username</label>
-                                            <input type="text" class="form-control" id="username" placeholder="Enter username">
+                                            <label for="email" class="form-label">Correo electrónico</label>
+                                            <input type="email" class="form-control" id="email" placeholder="Escribe aquí tu correo electrónico">
                                         </div>
 
+                                        <!-- A lo de contraseña no le cambié nada de los datos del for, name, id ni nada de eso -->
                                         <div class="mb-3">
+                                            <label class="form-label" for="password-input">Contraseña</label>
                                             <div class="float-end">
-                                                <a href="auth-pass-reset-basic.html" class="text-muted">Forgot password?</a>
+                                                <a href="" class="text-muted">¿Olvidaste tu contraseña?</a>
                                             </div>
-                                            <label class="form-label" for="password-input">Password</label>
                                             <div class="position-relative auth-pass-inputgroup mb-3">
-                                                <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="password-input">
+                                                <input type="password" class="form-control pe-5 password-input" placeholder="Escribe aquí tu contraseña" id="password-input">
                                                 <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted shadow-none password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                             </div>
                                         </div>
 
-                                        <div class="form-check">
+                                        <!-- No creo que se haga lo de "recordar mi usuario", verdad? Lo dejé comentado igual por si las dudas -->
+                                        <!-- <div class="form-check">
                                             <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
                                             <label class="form-check-label" for="auth-remember-check">Remember me</label>
-                                        </div>
+                                        </div> -->
 
                                         <div class="mt-4">
-                                            <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                            <button class="btn btn-success w-100" type="submit">Iniciar sesión</button>
                                         </div>
 
-                                        <div class="mt-4 text-center">
-                                            <div class="signin-other-title">
-                                                <h5 class="fs-13 mb-4 title">Sign In with</h5>
-                                            </div>
-                                            <div>
-                                                <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
-                                                <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
-                                                <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
-                                                <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
-                                            </div>
-                                        </div>
                                     </form>
                                 </div>
                             </div>
@@ -116,7 +106,7 @@
                         <!-- end card -->
 
                         <div class="mt-4 text-center">
-                            <p class="mb-0">Don't have an account ? <a href="auth-signup-basic.html" class="fw-semibold text-primary text-decoration-underline"> Signup </a> </p>
+                            <p class="mb-0">¿Aún no tienes una cuenta? <a href="" class="fw-semibold text-primary text-decoration-underline"> Regístrate </a> </p>
                         </div>
 
                     </div>
